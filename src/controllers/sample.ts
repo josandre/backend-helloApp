ebe1cd245afb07d14765eede1e322a7f882d37b3
Jocselyn Aguilar <ajocseline@gmail.com>
2023-06-12 20:24:20 -0600

    fix tsconfig

--- src/controllers/sample.ts
+++ src/controllers/sample.ts
@@ -9,7 +9,7 @@ export default class SampleController {
     @Get("/")
     public async holaMundo(): Promise<HolaMundoResponse> {
         return {
-            message: "Hola mundo!",
+            message: "hello world!",
         };
     }
 }
