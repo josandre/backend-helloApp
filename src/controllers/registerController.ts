3d60ca1fc4d7045a214738b460793cbf40b4befb
Jocselyn Aguilar <ajocseline@gmail.com>
2023-07-09 12:18:43 -0600

    mapping name user

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -1,7 +1,4 @@
 import {Body, Get, Post, Query, Route} from "tsoa";
-import jwt from "jsonwebtoken";
-import config from "../../config";
-import {LogInResponse} from "../models/LogInResponse";
 import {UserRequest} from "../models/UserRequest";
 const Models = require('../../models');
 const User = Models.User;
@@ -11,7 +8,7 @@ export default class RegisterController {
 
     @Post("/")
     public async register(@Body()req:UserRequest): Promise<any>{
-        const user = await User.build({...req}).save();
+        const user = await User.build({...req, firstName: req.name}).save();
 
         if(user)
         {
