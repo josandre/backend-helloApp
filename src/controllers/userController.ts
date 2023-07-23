426120e22c12640b8d5ccdfc8145d8dfbb8f6732
gracielamendezrojas <mmendezr@ucenfotec.ac.cr>
2023-07-23 10:27:37 -0600

    show image (only PNG)

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -8,6 +8,7 @@ const User = Models.User;
 @Route("login")
 export default class UserController {
 
+
     @Get("/")
     public async login(@Query() email, @Query() password): Promise<LogInResponse>{
         const user = await User.findOne({where: {email: email, password: password}});
@@ -20,7 +21,7 @@ export default class UserController {
                }
                );
            return new LogInResponse({
-               token
+               token, avatar: Buffer.from(user.avatar).toString('base64')
            })
         }
 
